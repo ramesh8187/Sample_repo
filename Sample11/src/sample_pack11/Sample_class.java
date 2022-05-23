d9ddb929f1db1477c79e13be145016b7c2c15050
rames <rames@LAPTOP-D2VADI3R>
2022-05-23 10:29:41 +0530

    by my own

---
+++ Sample11/src/sample_pack11/Sample_class.java
@@ -0,0 +1,35 @@
+package sample_pack11;
+
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
+public class Sample_class {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> li= new LinkedList<String>();
+		li.add("10");
+		li.add("20");
+		li.add("30");
+		li.add("40");
+		System.out.println("linked list size=="+li.size());
+		System.out.println("last elemrnt in linked list=="+li.getLast());
+		
+		
+		Set<String> hs=new LinkedHashSet<String> ();
+		hs.add("alpha");
+		hs.add("beta");
+		hs.add("gama");
+		
+		
+		
+		System.out.println("hashset=="+hs);
+             hs.addAll(li);	
+             System.out.println("hashset size=="+hs.size());
+             System.out.println("hashset+ linkedlist=="+hs);
+             
+
+	}
+
+}
